2277bb3bb90fce603a09fbc1507e165325779500
Manoj Chowdary <manojchow72@gmail.com>
2023-06-29 18:34:57 +0530

    Add files via upload

---
+++ src/main/java/com/example/quizapp/controllers/QuestionController.java
@@ -0,0 +1,41 @@
+package com.example.quizapp.controllers;
+
+
+import com.example.quizapp.model.Question;
+import com.example.quizapp.services.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("questions")
+@SuppressWarnings("unused")
+public class QuestionController {
+
+    @Autowired
+    private final QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+
+    @GetMapping("allQuestions")
+    public ResponseEntity<List<Question>> getQuestions() {
+        return questionService.getQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable String category) {
+        return questionService.getQuestionByCategory(category);
+    }
+
+    @PostMapping("addQuestion")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+    }
+
+
+}
