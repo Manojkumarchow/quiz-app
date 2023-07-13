867da16db6016ef513c102dfb3e5ea4931135b85
Manoj Chowdary <manojchow72@gmail.com>
2023-07-13 15:40:07 +0530

    Cache Entry -- Did a demo call only for persons

--- src/main/java/com/example/dormnestapp/QuizApplication.java
+++ src/main/java/com/example/dormnestapp/QuizApplication.java
@@ -2,10 +2,12 @@ package com.example.dormnestapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 
 @SpringBootApplication
 @EnableSpringHttpSession
+@EnableCaching
 public class QuizApplication {
 
     public static void main(String[] args) {
