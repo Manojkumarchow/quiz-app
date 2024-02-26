628c2001cb89a7c7f4f59bd570302ac66a6c3214
Manoj Chowdary <manojchow72@gmail.com>
2024-02-26 07:03:02 +0530

    Removed Caching and Redis properties (#8)

--- src/main/java/com/example/dormnestapp/DormNestApp.java
+++ src/main/java/com/example/dormnestapp/DormNestApp.java
@@ -2,16 +2,15 @@ package com.example.dormnestapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 
 @SpringBootApplication
 @EnableSpringHttpSession
-@EnableCaching
-public class QuizApplication {
+//@EnableCaching
+public class DormNestApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(QuizApplication.class, args);
+        SpringApplication.run(DormNestApp.class, args);
     }
 
 }
